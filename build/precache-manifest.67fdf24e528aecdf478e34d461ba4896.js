db494d5022738e029ec33529711021d02b7dc5f8
gp <gp.globaliasoft@gmail.com>
2020-12-10 15:22:21 +0530

    intial commits

---
+++ build/precache-manifest.67fdf24e528aecdf478e34d461ba4896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6932864083b9bd141223c11a474f674a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75158096bd8fb68e221b",
+    "url": "/static/css/main.0de4a901.chunk.css"
+  },
+  {
+    "revision": "4cbc4ab54b2beaa8ea87",
+    "url": "/static/js/2.582b55df.chunk.js"
+  },
+  {
+    "revision": "d0c7295dade8792d4ec795e8fb19eb02",
+    "url": "/static/js/2.582b55df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75158096bd8fb68e221b",
+    "url": "/static/js/main.4a5f3e2f.chunk.js"
+  },
+  {
+    "revision": "65ecc29e4a572716dab7",
+    "url": "/static/js/runtime-main.c493cd3b.js"
+  }
+]);
